d18c05a08f4c95bd0b8eadd38bd9e1542022bad5
jingfelix <jingfelix@outlook.com>
2025-06-04 22:47:10 +0800

    feat: complete with dify-easy!
    Signed-off-by: jingfelix <jingfelix@outlook.com>

--- provider/pushover.py
+++ provider/pushover.py
@@ -3,12 +3,14 @@ from typing import Any
 from dify_plugin import ToolProvider
 from dify_plugin.errors.tool import ToolProviderCredentialValidationError
 
+from src.pushover import *
+
 
 class DifyPluginPdmTemplateProvider(ToolProvider):
     def _validate_credentials(self, credentials: dict[str, Any]) -> None:
         try:
-            """
-            IMPLEMENT YOUR VALIDATION HERE
-            """
+            plugin = PushoverPlugin(credentials=PushoverCredentials(**credentials))
+            plugin.verify()  # Call the verification method to validate credentials
+
         except Exception as e:
             raise ToolProviderCredentialValidationError(str(e))
